f1bdcfd2e502f524e0cc059494555665474008ec
Yingying Zhang <noreply>
2023-04-27 19:15:31 -0700

    Implement the Book Details View

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'bookr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -110,6 +110,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -118,7 +120,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,7 +1,7 @@
 from django.contrib import auth
 from django.db import models
 
-# Create your models here.
+
 class Publisher(models.Model):
     """A company that publishes books."""
     name = models.CharField(max_length=50,
@@ -56,11 +56,12 @@ class BookContributor(models.Model):
 
 class Review(models.Model):
     content = models.TextField(help_text="The Review text.")
-    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    rating = models.IntegerField(help_text="The the reviewer has given.")
     date_created = models.DateTimeField(auto_now_add=True,
                                         help_text="The date and time the review was created.")
     date_edited = models.DateTimeField(null=True,
-                                       help_text="The date and time the review was last edited.")
+                                       help_text='''The date and time the review was last edited.'''
+                                       )
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE,
-                             help_text="The Book that this review is for.") 
+                             help_text="The Book that this review is for.")
