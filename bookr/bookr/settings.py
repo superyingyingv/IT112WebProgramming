e4a0898c925955658b5d72a54a5dad4785627894
Yingying Zhang <noreply>
2023-05-14 21:52:26 -0700

    Unit 6 Activity

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bookr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
