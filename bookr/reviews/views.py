23727804f383441fc75ed922913f23ea6d99d5bd
Yingying Zhang <noreply>
2023-05-20 23:39:16 -0700

    Activity 7 Book Search

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Book
+from .models import Book, Contributor
 from .utils import average_rating
-
+from .forms import SearchForm
 
 def index(request):
     return render(request, "base.html")
@@ -10,7 +10,31 @@ def index(request):
 
 def book_search(request):
     search_text = request.GET.get("search", "")
-    return render(request, "reviews/search-results.html", {"search_text": search_text})
+    form = SearchForm(request.GET)
+    books = set()
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+            fname_contributors = \
+                Contributor.objects.filter(first_names__icontains=search)
+
+            for contributor in fname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
+
+            lname_contributors = \
+                Contributor.objects.filter(last_names__icontains=search)
+
+            for contributor in lname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
+
+    return render(request, "reviews/search-results.html", {"form": form, "search_text": search_text, "books": books})
 
 
 def book_list(request):
