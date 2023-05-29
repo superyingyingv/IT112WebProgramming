184a78e63df8963a1fa4827cbb479da1fe19e623
Yingying Zhang <noreply>
2023-05-28 22:23:52 -0700

    Assignment 8

--- bookr/reviews/forms.py
+++ bookr/reviews/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from .models import Publisher, Review
+
+
 class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
     search_in = forms.ChoiceField(required=False,
@@ -7,3 +10,17 @@ class SearchForm(forms.Form):
                                       ("title", "Title"),
                                       ("contributor", "Contributor")
                                   ))
+
+
+class PublisherForm(forms.ModelForm):
+    class Meta:
+        model = Publisher
+        fields = "__all__"
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ["date_edited", "book"]
+
+    rating = forms.IntegerField(min_value=0, max_value=5)
--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('books/', views.book_list, name='book_list'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
+    path('books/<int:book_pk>/reviews/<int:review_pk>/', views.review_edit, name='review_edit'),
+    path('book-search/', views.book_search, name='book_search'),
+    path('publishers/<int:pk>/',views.publisher_edit, name='publisher_edit'),
+    path('publishers/new/',views.publisher_edit, name='publisher_create'),
 ]
-
